8cd70fcb4dd711f7a16825e9ed78839495f1f93d
Wesley-waka <wesleywaka77@gmail.com>
2024-03-23 12:02:29 +0300

    Implement client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,15 +3,53 @@ import React from 'react'
 import {TextField,Button} from '@radix-ui/themes';
 import "easymde/dist/easymde.min.css";
 import SimpleMDE from 'react-simplemde-editor';
+import {useForm,Controller} from 'react-hook-form';
+import axios from 'axios'
+import {zodResolver} from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchemas';
+import {z} from 'zod';
+
+type IssueForm = z.infer <typeof createIssueSchema>;
+
+interface IssueForm{
+    title: string;
+    description: string;
+}
+
 
 const page = () => {
-  return (
-    <div className='max-w-xl space-y-3'>
+    const router = useRouter();
+    const {register,control,handleSubmit, formState: {errors}} = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema)
+    });
+    const [error,setError] = useState('');
+
+    return (
+        <div className='max-w-xl'>
+            {error && 
+            <Callout.Root color='red' className='mb-5/06    '>
+                <Callout.Text>{error}</Callout.Text>
+            </Callout.Root>}
+    <form onSubmit={handleSubmit(async (data) => {
+        try {
+            await axios.post('/api/issues',data);
+            router.push('/issues');
+        } catch (error) {
+            setError('An unexpected error occurred');
+        } })}
+    className='max-w-xl space-y-3'>
       <TextField.Root>
-        <TextField.Input placeholder='Title'/>
+        <TextField.Input placeholder='Title' {...register('title')}/>
       </TextField.Root>
-      <SimpleMDE placeholder='Description'/>
+      {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
+      <Controller
+        name="description"
+        control={control}
+        render={({field})=><SimpleMDE placeholder='Description' {...field}/>}
+      />
+      {errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
       <Button>Submit New Issue</Button>
+    </form>
     </div>
   )
 }
