ba61c35ae17505d8990d64b399ac5c1fb68c10a0
Wesley-waka <wesleywaka77@gmail.com>
2024-03-23 22:42:24 +0300

    Refactor :Move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,15 +10,30 @@ import { createIssueSchema } from '@/app/validationSchemas';
 import {z} from 'zod';
 import { useRouter } from 'next/router';
 import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
+
+
 
 type IssueForm = z.infer <typeof createIssueSchema>;
 
 const Page = () => {
+    const [isSubmitting,setSubmitting] = useState(false);
     const router = useRouter();
     const {register,control,handleSubmit, formState: {errors}} = useForm<IssueForm>({
         resolver: zodResolver(createIssueSchema)
     });
     const [error,setError] = useState('');
+
+    const onSubmit = handleSubmit(async (data) => {
+      try {
+        setSubmitting(true);
+        // createIssue(data);
+          await axios.post('/api/issues',data);
+          router.push('/issues');
+      } catch (error) {
+        setSubmitting(false);
+          setError('An unexpected error occurred');
+      } });
 
     return (
         <div className='max-w-xl'>
@@ -26,13 +41,7 @@ const Page = () => {
             <Callout.Root color='red' className='mb-5/06    '>
                 <Callout.Text>{error}</Callout.Text>
             </Callout.Root>}
-    <form onSubmit={handleSubmit(async (data) => {
-        try {
-            await axios.post('/api/issues',data);
-            router.push('/issues');
-        } catch (error) {
-            setError('An unexpected error occurred');
-        } })}
+    <form onSubmit={onSubmit}
     className='max-w-xl space-y-3'>
       <TextField.Root>
         <TextField.Input placeholder='Title' {...register('title')}/>
@@ -50,7 +59,7 @@ const Page = () => {
         )}
       />
       <ErrorMessage>{errors.description?.message}</ErrorMessage>
-      <Button>Submit New Issue</Button>
+      <Button disabled={isSubmitting}>Submit New Issue{ isSubmitting && <Spinner/>}</Button>
     </form>
     </div>
   )
