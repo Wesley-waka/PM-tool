60854d589cf39627874b0947bc43f4446cc69c9c
Wesley-waka <wesleywaka77@gmail.com>
2024-03-26 20:54:09 +0300

    solve rendering error

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -2,18 +2,30 @@ import prisma from '@/prisma/client';
 import { Table } from '@radix-ui/themes';
 import delay from 'delay';
 import IssueActions from './IssueActions';
-import {IssueStatusBadge,Link} from '@/app/components';
-import { Status } from '@prisma/client';
+import { IssueStatusBadge, Link } from '@/app/components';
+import { Issue, Status } from '@prisma/client';
+import NextLink from 'next/link';
+import { ArrowUpIcon } from '@radix-ui/react-icons';
 
 interface Props {
-  searchParams: {status: Status}
+  searchParams: { status: Status }
 }
 
-const page = async({
+const page = async ({
   searchParams
-  }: {
-    searchParams: {status:  Status };
-  }) => {
+}: {
+  searchParams: { status: Status, orderBy: keyof Issue };
+}) => {
+
+  const columns: {
+    label: string;
+    value: keyof Issue;
+    className?: string;
+  }[] = [
+      { label: 'Issue', value: 'title', className: "hidden md:table-cell" },
+      { label: 'Status', value: 'status', className: "hidden md:table-cell" },
+      { label: 'Created', value: 'createdAt', className: "hidden md:table-cell" },
+    ]
   // console.log(searchParams.status)
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status) ? searchParams.status : undefined;
@@ -24,16 +36,18 @@ const page = async({
     }
   });
   // await delay(2000);
-  
+
   return (
     <div>
       <IssueActions />
-    <Table.Root variant='surface'>
-      <Table.Header>
-        <Table.Row>
-          <Table.ColumnHeaderCell className='hidden md:table-cell'>Issue</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+      <Table.Root variant='surface'>
+        <Table.Header>
+          <Table.Row>
+          {columns.map(column => (<Table.ColumnHeaderCell key={column.value}>
+                <NextLink href={{query: {...searchParams, orderBy: column.value}}}>{column.label}</NextLink>
+                  {column.value === searchParams.orderBy && <ArrowUpIcon className='inline' /> }
+              </Table.ColumnHeaderCell>
+          ))}
         </Table.Row>
       </Table.Header>
       <Table.Body>
@@ -44,18 +58,18 @@ const page = async({
                 {issue.title}
               </Link>
               <div className="block md:hidden">
-                <IssueStatusBadge status={issue.status}/>
+                <IssueStatusBadge status={issue.status} />
               </div>
             </Table.Cell>
             <Table.Cell className='hidden md:table-cell'>
-              <IssueStatusBadge status={issue.status}/>
+              <IssueStatusBadge status={issue.status} />
             </Table.Cell>
             <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
           </Table.Row>
         )))}
       </Table.Body>
     </Table.Root>
-    </div>
+    </div >
   )
 };
 
